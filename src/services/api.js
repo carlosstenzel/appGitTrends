9da16a9d3501adf6c7eaf87fcb4ae934b9aaa7fc
carlosstenzel <carlosstenzel@hotmail.com>
2019-12-26 21:32:08 -0300

    Alter interaction with .env

--- src/services/api.js
+++ src/services/api.js
@@ -6,23 +6,10 @@ const api = new ApolloClient({
   request: operation => {
     operation.setContext({
       headers: {
-        authorization: `Bearer 701db60cf75ef19cc89698dade30141673468014`,
-      },
-    });
-  },
-});
-/*
-const api = new ApolloClient({
-  uri: 'https://api.github.com/graphql',
-  request: operation => {
-    operation.setContext({
-      headers: {
         authorization: `Bearer ${KEY_API_GITHUB}`,
       },
     });
   },
 });
-
-*/
 
 export default api;
